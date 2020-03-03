8bb92b00be0b42123be6bb58fa09df6ba75b6940
Winter Runion <winterrunion@gmail.com>
2020-03-03 11:11:51 -0800

    Add test to check for presence of an argument in the calculateAge function.

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -13,9 +13,14 @@ describe('Mercury', () => {
     expect(mercury.earthYearPercentage).toEqual(.24);
   });
 
-  test(`Should have a calculateAge function`, () => {
+  test(`Should have a calculateAge method`, () => {
     let mercury = new Mercury('Mercury');
     expect(mercury.calculateAge()).toBeTruthy();
+  });
+
+  test(`calculateAge method take in an argument, and return it unchanged`, () => {
+    let mercury = new Mercury('Mercury');
+    expect(mercury.calculateAge(10)).toEqual(10);
   });
 
 });
