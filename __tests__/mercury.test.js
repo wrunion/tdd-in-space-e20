e9f8b689f40e0dec1079d0f97328baf5b23e7d70
Winter Runion <winterrunion@gmail.com>
2020-03-03 11:17:08 -0800

    Refactor Mercury test suite to use beforeEach() 'setup' syntax.

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -2,24 +2,24 @@
 import { Mercury } from './../src/mercury';
 
 describe('Mercury', () => {
+  let mercury;
 
+  beforeEach(() => {
+    mercury = new Mercury('Mercury');
+  });
+ 
   test(`Should correctly create a Mercury class with a constructor function and name property`, () => {
-    let mercury = new Mercury('Mercury');
     expect(mercury.name).toEqual('Mercury');
   });
 
-  test(`Should have "earthYearPercentage" property that returns .24`, () => {
-    let mercury = new Mercury('Mercury');
-    expect(mercury.earthYearPercentage).toEqual(.24);
+  test(`Should have "earthYearPercentage" property that returns .24`, () => {    expect(mercury.earthYearPercentage).toEqual(.24);
   });
 
   test(`Should have a calculateAge method`, () => {
-    let mercury = new Mercury('Mercury');
     expect(mercury.calculateAge(10)).toBeTruthy();
   });
 
-  test(`calculateAge method take in an argument, and return it unchanged`, () => {
-    let mercury = new Mercury('Mercury');
+  test(`Should have a calculateAge method that takes in an argument, and return it unchanged`, () => {
     expect(mercury.calculateAge(10)).toEqual(10);
   });
 
