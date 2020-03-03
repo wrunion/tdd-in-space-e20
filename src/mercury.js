d8bf785ac3d7be804629944f6c252a5b903bc3e7
Winter Runion <winterrunion@gmail.com>
2020-03-03 12:54:48 -0800

    Add branch to lifeExpectancy function to handle negative numbers. Test now passes.

--- src/mercury.js
+++ src/mercury.js
@@ -11,12 +11,14 @@ export class Mercury {
 
   lifeExpectancy(earthAge, earthLifeExpectancy) {
     let mercuryLifeExpectancy = (earthLifeExpectancy - earthAge) / this.earthYearPercentage;
-    if (mercuryLifeExpectancy <= 5) {
-      return `Time's almost up! You have ${mercuryLifeExpectancy} years left on ${this.planetName}`;
-    } else if (mercuryLifeExpectancy < 0) {
-      return `Congrats! You've already beat father time! You've lived ${mercuryLifeExpectancy} extra years left on ${this.planetName}!`;
+    // if (mercuryLifeExpectancy <= 5) {
+    //   return `Time's almost up! You have ${mercuryLifeExpectancy} years left on ${this.planetName}`;
+    // }  
+    if (mercuryLifeExpectancy < 0) {
+      return Math.abs(mercuryLifeExpectancy);
     }
-
+    //   return `Congrats! You beat father time! You've lived ${Math.abs(mercuryLifeExpectancy)} extra years left on ${this.planetName}!`;
+    // }
     return mercuryLifeExpectancy;
   }
 }
