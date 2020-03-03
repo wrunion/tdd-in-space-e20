aced734ed90955957b61baa182779a1314ecb95c
Winter Runion <winterrunion@gmail.com>
2020-03-03 10:41:37 -0800

    Reword test parameters to specify 'class' instead of only 'function.'

--- __tests__/mercury.test.js
+++ __tests__/mercury.test.js
@@ -1,9 +1,9 @@
 /* eslint-disable no-undef */
-import { Mercury } from '/./../src/mercury';
+import { Mercury } from '/./../src/mercury.js';
 
 describe('Mercury', () => {
 
-  test(`Should correctly create a Mercury object with a name property`, () => {
+  test(`Should correctly create a Mercury class with a constructor function and name property`, () => {
     let mercury = new Mercury('Mercury');
     expect(mercury.name).toEqual('Mercury');
   });
