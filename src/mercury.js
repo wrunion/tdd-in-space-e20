79480806f8530b602c39149f05c37b2a20310ba3
wrunion <winterrunion@gmail.com>
2020-03-05 12:35:06 -0800

    Clean up file.

--- src/mercury.js
+++ src/mercury.js
@@ -1,4 +1,3 @@
-/* eslint-disable no-console */
 export class Mercury {
   constructor(planetName) {
     this.planetName = planetName;
@@ -10,15 +9,10 @@ export class Mercury {
   }
 
   lifeExpectancy(earthAge, earthLifeExpectancy) {
-    let mercuryLifeExpectancy = (earthLifeExpectancy - earthAge) / this.earthYearPercentage;
-    // if (mercuryLifeExpectancy <= 5) {
-    //   return `Time's almost up! You have ${mercuryLifeExpectancy} years left on ${this.planetName}`;
-    // }  
+    let mercuryLifeExpectancy = (earthLifeExpectancy - earthAge) / this.earthYearPercentage;  
     if (mercuryLifeExpectancy < 0) {
       return Math.abs(mercuryLifeExpectancy);
     }
-    //   return `Congrats! You beat father time! You've lived ${Math.abs(mercuryLifeExpectancy)} extra years left on ${this.planetName}!`;
-    // }
     return mercuryLifeExpectancy;
   }
 }
