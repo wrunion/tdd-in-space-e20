2c3f0988396732384b2915b5743f558c38871fe8
Winter Runion <winterrunion@gmail.com>
2020-03-05 13:53:22 -0800

    Clean up and comment.

--- src/person.js
+++ src/person.js
@@ -4,11 +4,11 @@ export class Person {
     this.age = age;
     this.gender = gender;
   }
-  /* The numbers below are based on the worldwide WHO statistics for life expectancy around the globe, broken down by gender. */ 
+  /* Based on worldwide WHO statistics */ 
   earthLifeExpectancy() {
     if (this.gender === 'female') { return 74; }
     if (this.gender === 'male') { return 70; }
     if (this.gender === 'other') { return 72; }
-    return 72; // Default value based on the average life expectancy in 2016, according to WHO (World Health Organization.) It's also the value I used for "other" genders, since we don't have data for nonbinary gender life expectancy right now.
+    return 72; // Default value, equal to WHO average life expectancy globally
   }
 }
