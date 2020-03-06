d4945fa0b058b007a08ebe2be63de357488685dd
Winter Runion <winterrunion@gmail.com>
2020-03-05 23:56:52 -0800

    Refactor UI.

--- src/main.js
+++ src/main.js
@@ -1,5 +1,5 @@
 import { Person } from './person';
-import { Planet } from './planet';
+// import { Planet } from './planet';
 import './styles.css';
 
 // const planetPercentages = {
@@ -9,23 +9,40 @@ import './styles.css';
 //   jupiter: 11.86
 // }
 
+// let planets = [];
+
 $(document).ready(function() {
 
   $("form").submit(function(event) {
     const name = $("input#name").val();
     const userAge = $("input#userAge").val();
     const gender = $("select#gender").val();
-    let planets = [];
+    let planetsChecked = [];
     $("input:checkbox[name=planets]:checked").each(function() {
       let planet = $(this).val();
-      planets.push(planet);
+      planetsChecked.push(planet);
     });
     
     const user = new Person(name, userAge, gender);
     const lifeExpectancy = user.earthLifeExpectancy();
 
+    // if (planets.includes('mercury')) { 
+    //   const mercury = new Planet('Mercury', .24, userAge, lifeExpectancy);
+    //   planetArray.push(mercury);
+    //   console.log(mercury);
+    //   console.log(planetArray);
+    // }
+
+
+    // planets.forEach(function(planet) {
+    //   const planetPercentage = planetPercentages[planet];
+    //   let planetObj = new Planet(planet, planetPercentage, userAge, lifeExpectancy); 
+    //   console.log(planetObj);
+    // });
+
     console.log(user);
     console.log(lifeExpectancy);
+    console.log(planetsChecked);
     event.preventDefault();
   });
 
