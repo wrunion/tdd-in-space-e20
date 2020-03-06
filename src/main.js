fee93e9938cacdecf3251c18f7278c13e61de72a
Winter Runion <winterrunion@gmail.com>
2020-03-06 00:26:35 -0800

    Create Planet object from inputs.

--- src/main.js
+++ src/main.js
@@ -1,48 +1,39 @@
 import { Person } from './person';
 // import { Planet } from './planet';
 import './styles.css';
-
-// const planetPercentages = {
-//   mercury: .24,
-//   venus: .62,
-//   mars: 1.88,
-//   jupiter: 11.86
-// }
+import { Planet } from './planet';
 
 // let planets = [];
+
+/* Globally available reference variable */ 
+const planetPercentages = {
+  mercury: .24,
+  venus: .62,
+  mars: 1.88,
+  jupiter: 11.86
+};
 
 $(document).ready(function() {
 
   $("form").submit(function(event) {
+    /* Gather input from form fields */
     const name = $("input#name").val();
     const userAge = $("input#userAge").val();
     const gender = $("select#gender").val();
-    let planetsChecked = [];
-    $("input:checkbox[name=planets]:checked").each(function() {
-      let planet = $(this).val();
-      planetsChecked.push(planet);
-    });
+    const checkedPlanet = $("input:radio[name=planet]:checked").val();
     
+    /* Create Person object using inputted data */ 
     const user = new Person(name, userAge, gender);
-    const lifeExpectancy = user.earthLifeExpectancy();
+    const earthLifeExpectancy = user.earthLifeExpectancy();
 
-    // if (planets.includes('mercury')) { 
-    //   const mercury = new Planet('Mercury', .24, userAge, lifeExpectancy);
-    //   planetArray.push(mercury);
-    //   console.log(mercury);
-    //   console.log(planetArray);
-    // }
+    /* Create Planet object */
+    const checkedPlanetPercentage = planetPercentages[checkedPlanet];
+    const planet = new Planet(checkedPlanet, checkedPlanetPercentage, userAge, earthLifeExpectancy);
 
-
-    // planets.forEach(function(planet) {
-    //   const planetPercentage = planetPercentages[planet];
-    //   let planetObj = new Planet(planet, planetPercentage, userAge, lifeExpectancy); 
-    //   console.log(planetObj);
-    // });
-
+    console.log(planet.calculateAge());
     console.log(user);
-    console.log(lifeExpectancy);
-    console.log(planetsChecked);
+    console.log(earthLifeExpectancy);
+    console.log(planet);
     event.preventDefault();
   });
 
