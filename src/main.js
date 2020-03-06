7a63adbd11f3e3dcb78d974e5d8b5cb7072b81b2
Winter Runion <winterrunion@gmail.com>
2020-03-05 16:32:57 -0800

    Write ui logic to get form values.

--- src/main.js
+++ src/main.js
@@ -1,7 +1,17 @@
-$('.ui.checkbox')
-  .checkbox()
-;
 $(document).ready(function() {
 
+  $("form").submit(function(event) {
+    let name = $("input#name").val();
+    let userAge = $("input#userAge").val();
+    let gender = $("select#gender").val();
+    let planets = [];
+    $("input:checkbox[name=planets]:checked").each(function() {
+      let planet = $(this).val();
+      planets.push(planet);
+    });
+    
+    console.log(name, userAge, gender, planets);
+    event.preventDefault();
+  });
 
 });
