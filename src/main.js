12eb2439cee3652f9eca68ebc8b1ba0227e1660c
Winter Runion <winterrunion@gmail.com>
2020-03-06 23:46:35 -0800

    Finish formatting content div.

--- src/main.js
+++ src/main.js
@@ -10,6 +10,8 @@ const planetPercentages = {
   jupiter: 11.86
 };
 
+let planetsPicked = [];
+
 $(document).ready(function() {
 
   $("form").submit(function(event) {
@@ -18,23 +20,28 @@ $(document).ready(function() {
     const userAge = $("input#userAge").val();
     const gender = $("select#gender").val();
     const checkedPlanet = $("input:radio[name=planet]:checked").val();
+    /* Check for duplicate planet input, and ignore planets that have already been submitted */
+    if (planetsPicked.includes(checkedPlanet) === false) {
+      planetsPicked.push(checkedPlanet);
     
-    /* Create Person object using inputted data */ 
-    const user = new Person(name, userAge, gender);
-    const earthLifeExpectancy = user.earthLifeExpectancy();
+      /* Create Person object using inputted data */ 
+      const user = new Person(name, userAge, gender);
+      const earthLifeExpectancy = user.earthLifeExpectancy();
 
-    /* Create Planet object */
-    const checkedPlanetPercentage = planetPercentages[checkedPlanet];
-    const planet = new Planet(checkedPlanet, checkedPlanetPercentage, userAge, earthLifeExpectancy);
+      /* Create Planet object */
+      const checkedPlanetPercentage = planetPercentages[checkedPlanet];
+      const planet = new Planet(checkedPlanet, checkedPlanetPercentage, userAge, earthLifeExpectancy);
 
-    /* Uppercase the planet name for display */
-    const planetDisplayName = checkedPlanet.replace(/^\w/, c => c.toUpperCase());
+      /* Uppercase the planet name for display */
+      const planetDisplayName = checkedPlanet.replace(/^\w/, c => c.toUpperCase());
 
-    /* Return the calculated data back to the user */
-    $("#id-section").hide();
-    $("#results-div").append(`<div class="field results-text">You are <strong>${planet.calculateAge()}</strong> years old on ${planetDisplayName}! 
+      /* Return the calculated data back to the user */
+      $("#id-section").hide();
+      $("#results-div").append(`<div class="field results-text">You are <strong>${planet.calculateAge()}</strong> years old on ${planetDisplayName}! 
     </div>`);
-    $("#results-div").show(); 
+      $("#results-div").show(); 
+    }
+
     event.preventDefault();
   });
 
